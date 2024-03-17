c2b2316d77b4bb342a109ae59790da3a87419653
gorillaKim <yhkim@madup.com>
2024-03-17 20:50:12 +0900

    feat: 메모 삭제기능 구현

--- lib/memoList.dart
+++ lib/memoList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Memo {
   Memo(String _content) {
@@ -7,19 +8,8 @@ class Memo {
   }
 
   String content = '';
-  bool? deleted = false;
   DateTime? created_at;
   DateTime? updated_at;
-
-  void delete() {
-    deleted = false;
-    updated_at = DateTime.now();
-  }
-
-  void restore() {
-    deleted = true;
-    updated_at = DateTime.now();
-  }
 
   void edit(String _content) {
     content = _content;
@@ -38,6 +28,7 @@ class MemoList extends StatefulWidget {
 
 class _MemoListState extends State<MemoList> {
   final readList = <Memo>{};
+  final deletedList = <Memo>{};
 
   void _handleMemoRead(Memo memo, bool read) {
     setState(() {
@@ -45,6 +36,18 @@ class _MemoListState extends State<MemoList> {
         readList.add(memo);
       } else {
         readList.remove(memo);
+      }
+    });
+  }
+
+  void _handleMemoDelete(Memo memo, bool delete) {
+    setState(() {
+      if (!delete) {
+        widget.memos.add(memo);
+        deletedList.remove(memo);
+      } else {
+        widget.memos.remove(memo);
+        deletedList.add(memo);
       }
     });
   }
@@ -61,7 +64,8 @@ class _MemoListState extends State<MemoList> {
           return MemoListItem(
               memo: memo,
               read: readList.contains(memo),
-              memoReadCallback: _handleMemoRead);
+              memoReadCallback: _handleMemoRead,
+              memoDeleteCallback: _handleMemoDelete);
         }).toList(),
       ),
     );
@@ -69,14 +73,19 @@ class _MemoListState extends State<MemoList> {
 }
 
 typedef MemoReadCallback = Function(Memo memo, bool read);
+typedef MemoDeleteCallback = Function(Memo memo, bool delete);
 
 class MemoListItem extends StatelessWidget {
   MemoListItem(
-      {required this.memo, required this.read, required this.memoReadCallback});
+      {required this.memo,
+      required this.read,
+      required this.memoReadCallback,
+      required this.memoDeleteCallback});
 
   final Memo memo;
   final bool read;
   final MemoReadCallback memoReadCallback;
+  final MemoDeleteCallback memoDeleteCallback;
 
   Color _getColor(BuildContext context) {
     return read //
@@ -96,6 +105,7 @@ class MemoListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      isThreeLine: true,
       onTap: () {
         memoReadCallback(memo, read);
       },
@@ -106,6 +116,46 @@ class MemoListItem extends StatelessWidget {
       title: Text(
         memo.content,
         style: _getTextStyle(context),
+      ),
+      subtitle: Text(
+        DateFormat('yyyy-MM-dd hh:mm')
+            .format((memo.updated_at ?? memo.created_at) as DateTime),
+        style: _getTextStyle(context),
+      ),
+      trailing: PopupMenuButton<String>(
+        onSelected: (String? value) {
+          if (value == 'delete') {
+            memoDeleteCallback(memo, true);
+          }
+        },
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          const PopupMenuItem<String>(
+            value: 'edit',
+            child: Row(
+              children: [
+                Icon(Icons.edit),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text(
+                    '수정',
+                  ),
+                )
+              ],
+            ),
+          ),
+          const PopupMenuItem<String>(
+            value: 'delete',
+            child: Row(
+              children: [
+                Icon(Icons.delete),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Text('삭제'),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
