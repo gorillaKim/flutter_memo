4a30d4d69d805577bc8a716a5c5e74d0197a4021
gorillaKim <yhkim@madup.com>
2024-03-17 19:45:08 +0900

    feat: 메모 리스트 위젯 개발

--- lib/memoList.dart
+++ lib/memoList.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/material.dart';
+
 class Memo {
-  Memo(this.memo) {
-    created_at = DateTime.now();
+  Memo(String _content) {
+    this.content = _content;
+    this.created_at = DateTime.now();
   }
 
-  String memo;
+  String content = '';
+  bool? deleted = false;
   DateTime? created_at;
   DateTime? updated_at;
-  bool deleted = false;
 
   void delete() {
     deleted = false;
@@ -18,8 +21,92 @@ class Memo {
     updated_at = DateTime.now();
   }
 
-  void edit(String newMemo) {
-    memo = newMemo;
+  void edit(String _content) {
+    content = _content;
     updated_at = DateTime.now();
+  }
+}
+
+class MemoList extends StatefulWidget {
+  const MemoList({required this.memos, super.key});
+
+  final List<Memo> memos;
+
+  @override
+  State<MemoList> createState() => _MemoListState();
+}
+
+class _MemoListState extends State<MemoList> {
+  final readList = <Memo>{};
+
+  void _handleMemoRead(Memo memo, bool read) {
+    setState(() {
+      if (!read) {
+        readList.add(memo);
+      } else {
+        readList.remove(memo);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Memo list'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        children: widget.memos.map((memo) {
+          return MemoListItem(
+              memo: memo,
+              read: readList.contains(memo),
+              memoReadCallback: _handleMemoRead);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+typedef MemoReadCallback = Function(Memo memo, bool read);
+
+class MemoListItem extends StatelessWidget {
+  MemoListItem(
+      {required this.memo, required this.read, required this.memoReadCallback});
+
+  final Memo memo;
+  final bool read;
+  final MemoReadCallback memoReadCallback;
+
+  Color _getColor(BuildContext context) {
+    return read //
+        ? Colors.black54
+        : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!read) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        memoReadCallback(memo, read);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(memo.content[0]),
+      ),
+      title: Text(
+        memo.content,
+        style: _getTextStyle(context),
+      ),
+    );
   }
 }
